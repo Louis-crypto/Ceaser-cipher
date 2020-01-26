ff2a4e45773d1666aeb16580e611ef6285e0cb7d
Louis-crypto <kinglouis745@gmail.com>
2020-01-26 10:54:18 +0300

    collect user message from the console using .readLine(); method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,5 +4,7 @@ public class App {
     public static  void main(String[] args){
         Console console = System.console();
         System.out.println("This is a Caesar Cipher encrypting app, Enter message you want to encrypt");
+        String userEnteredInput = console.readLine();
+
     }
 }
