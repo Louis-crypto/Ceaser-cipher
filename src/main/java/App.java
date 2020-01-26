2b30a7286fec2cab3bf7d5d6760ce21afeaa8782
Louis-crypto <kinglouis745@gmail.com>
2020-01-26 14:35:28 +0300

    create logic for calculating integer at a specific shift

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,11 @@ public class App {
         Console console = System.console();
         System.out.println("This is a Caesar Cipher encrypting app, Enter message you want to encrypt");
         String userEnteredInput = console.readLine();
-        String userInputToUpperCase = userEnteredInput.toUpperCase();
-        System.out.println("Enter by how many keys you want to shift your message between 1 and 25");
+        String userInput = userEnteredInput.toUpperCase();
+        System.out.println("Enter by how many keys you want to shift your message");
         String userEnteredKey = console.readLine();
+        int userKey = Integer.parseInt(userEnteredKey);
+        Cipher cipher = new Cipher(userInput, userKey);
+        System.out.println(cipher.encryptedText(userInput, userKey));
     }
 }
--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,39 +1,34 @@
 class Cipher {
-    private  String userInput;
+    private String userInput;
     private int userKey;
 
-    private String upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//    private String lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    private String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-public Cipher(String userInput, int userKey){
-    this.userInput = userInput;
-    this.userKey = userKey;
-}
-public String getUserInput(){
-    return this.userInput;
-}
-public int getUserKey(){
-    return this.userKey;
-}
+    public Cipher(String userInput, int userKey) {
+        this.userInput = userInput;
+        this.userKey = userKey;
+    }
 
-public String encodedText(String userInput, int userKey){
-    for(i = 0; i < userInput.length(); i++){
-    if(userKey > 25){
-        System.out.println("Please enter a valid Key");
-}else{
-        int shiftBy = userKey + positionOfLetter
+    public String getUserInput() {
+        return this.userInput;
+    }
+
+    public int getUserKey() {
+        return this.userKey;
+    }
+
+    public String encryptedText(String userInput, int userKey){
+        String encodedMessage = "";
+       for(int i=0; i < userInput.length(); i++){
+           char userInputCharacters = userInput.charAt(i);
+           int indexOfLetter = alphabet.indexOf(userInputCharacters);
+           int characterPosition = indexOfLetter + userKey;
+           if(characterPosition > 25){
+               encodedMessage += alphabet.charAt(characterPosition%26);
+           }else{
+               encodedMessage += alphabet.charAt(characterPosition);
+           }
+       }
+       return encodedMessage;
     }
 }
-}
-//    public static void main(String[] args) {
-//        Console console = System.console();
-//        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//        System.out.println("Enter your message");
-//        String userInput = console.readLine();
-//        String upperUserInput = userInput.toUpperCase();
-//        for (int i = 0; i <= upperUserInput.length(); i++) {
-//            char userInputCharacters = upperUserInput.charAt(i);
-//            // System.out.println(userInputCharacters);
-//            int keyOfLetter = alphabet.indexOf(userInputCharacters);
-//            int userKey = keyOfLetter + 5;
-//            System.out.println(userKey);
