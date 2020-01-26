eb562d47cde1d1b5a5c8552ef8504d428e45edae
Louis-crypto <kinglouis745@gmail.com>
2020-01-26 10:51:52 +0300

    added a prompt message to ask user to enter their input

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+import java.io.Console;
+
+public class App {
+    public static  void main(String[] args){
+        Console console = System.console();
+        System.out.println("This is a Caesar Cipher encrypting app, Enter message you want to encrypt");
+    }
+}
--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,6 +1,10 @@
-public class Cipher {
+class Cipher {
     private  String userInput;
     private int userKey;
+
+    private String upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private String lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+
 public Cipher(String userInput, int userKey){
     this.userInput = userInput;
     this.userKey = userKey;
@@ -11,4 +15,25 @@ public String getUserInput(){
 public int getUserKey(){
     return this.userKey;
 }
+
+public String encodedText(String userInput, int userKey){
+    for(i = 0; i < userInput.length(); i++){
+    if(userKey > 25){
+        System.out.println("Please enter a valid Key");
+}else{
+        int shiftBy = userKey + positionOfLetter
+    }
 }
+}
+//    public static void main(String[] args) {
+//        Console console = System.console();
+//        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//        System.out.println("Enter your message");
+//        String userInput = console.readLine();
+//        String upperUserInput = userInput.toUpperCase();
+//        for (int i = 0; i <= upperUserInput.length(); i++) {
+//            char userInputCharacters = upperUserInput.charAt(i);
+//            // System.out.println(userInputCharacters);
+//            int keyOfLetter = alphabet.indexOf(userInputCharacters);
+//            int userKey = keyOfLetter + 5;
+//            System.out.println(userKey);
